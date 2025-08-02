9ac224df5eff8543e3d7536790c36491a7dc6890
jeffng5 <jeffrey.ng51213@outlook.com>
2025-08-02 07:49:19 -0400

    init db

--- app.js
+++ app.js
@@ -8,24 +8,24 @@ app.use(cors());
 const pool = require('./db.js')
 
 
-// app.post('/api/count', async (req, res, next) => {
-//     try {
-//         const result = await pool.query(`UPDATE totals SET total = total + 1 RETURNING *`)
-//         let visitorCount = result.rows
-//         return res.json({visitorCount})
-//     } catch (err) {
-//         return next(err)
-//     }
-// })
-
 app.post('/api/count', async (req, res, next) => {
     try {
-        const result = await pool.query(`INSERT INTO totals (total) VALUES (1)`)
-        let ans = result.rows;
-        return res.json({ans})
-    } catch(err){
+        const result = await pool.query(`UPDATE totals SET total = total + 1 RETURNING *`)
+        let visitorCount = result.rows
+        return res.json({visitorCount})
+    } catch (err) {
         return next(err)
     }
 })
+
+// app.post('/api/count', async (req, res, next) => {
+//     try {
+//         const result = await pool.query(`INSERT INTO totals (total) VALUES (1)`)
+//         let ans = result.rows;
+//         return res.json({ans})
+//     } catch(err){
+//         return next(err)
+//     }
+// })
 
 module.exports = app;
